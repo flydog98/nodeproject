020d3b3450eb1171c4804f8e22a412580e7b0578
flydog98 <pyjpyj0825@gmail.com>
2022-10-03 08:59:04 +0900

    feat: s3 uploading routing and bussiness logic

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const express = require("express");
 const app = express();
 
 const apirouter = require("./routes/api");
+const s3router = require("./routes/s3router")
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 //Express 4.16.0버전 부터 body-parser의 일부 기능이 익스프레스에 내장 body-parser 연결
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apirouter);
+app.use("/upload", s3router);
 
 // http listen port 생성 서버 실행
 const server = app.listen(3000, () => {
